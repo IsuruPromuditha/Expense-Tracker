ce532e3ae03f9991405bcc2e69bcf76542b8a155
Isuru <isurupromuditha617@gmail.com>
2024-07-16 19:20:04 +0530

    Add project files.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } // CategoryId is foreign key
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
